fe18649d53af3666b7205933d326df4f9b9e47ac
MTVaught <MTVaught@users.noreply.github.com>
2019-04-13 14:59:26 -0500

    Add directory parsing

--- backup.pl
+++ backup.pl
@@ -22,3 +22,44 @@ my $cmd = "cd $rootdir/$subdir && $TAR_CMD $outdir/$subdir-$timestamp.$TAR_EXT .
 print $cmd . "\n";
 
 print  . "\n";
+
+my @file_list;
+GetFilesInDirectory(\@file_list, $ENV{'PWD'}."/test/weekly");
+
+foreach my $file (@file_list)
+{
+    print $file . "\n";
+}
+# Get all files in directory:
+
+sub GetFilesInDirectory
+{
+    my ($file_list, $path) = @_;
+    my $success = 1;
+    
+    my $dir;
+    opendir ($dir, $path) or $success = 0;
+    if($success == 1)
+    {
+        @$file_list = readdir($dir);
+        closedir($dir);
+    }
+    
+    if($success)
+    {
+        my $index = 0;
+        while($index < scalar(@$file_list))
+        {
+            my $file = $$file_list[$index];
+            if($file eq "." || $file eq "..")
+            {
+                splice(@$file_list, $index, 1);
+            }
+            else
+            {
+                $index++;
+            }
+        }
+    }
+    return $success;
+}
