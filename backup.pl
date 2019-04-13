bb5503e7daef46aa5795de4496bf8f43ef42c8a9
MTVaught <MTVaught@users.noreply.github.com>
2019-04-13 15:27:43 -0500

    Add function to create tar archive

--- backup.pl
+++ backup.pl
@@ -7,21 +7,21 @@ use POSIX;
 #   --rootdir=<src>
 #   --outdir=<dest>
 
+my $TAR = "tar";
+my $TAR_FLAGS = "cf";
+my $TAR_FLAGS_ENCRYPT = "czf";
+my $TAR_EXT = "tar";
+my $TAR_EXT_ENCRYPT = "tar.gz";
+
 my $rootdir = "/home/mtvaught";
 my $outdir = "/home/mtvaught";
 my $subdir = "tmp";
-my $TAR = "tar";
-my $TAR_FLAGS = "cf";
-my $TAR_EXT = "tar";
 
-my $TAR_CMD = "$TAR $TAR_FLAGS";
+my $encryption = 0;
+my $archive_path;
+my $success = CreateArchive(\$archive_path, $rootdir, $subdir, $outdir, $encryption);
+print "success = $success\n";
 
-my $timestamp = strftime "%Y-%m-%d_%H-%M-GMT", gmtime time;
-
-my $cmd = "cd $rootdir/$subdir && $TAR_CMD $outdir/$subdir-$timestamp.$TAR_EXT .";
-print $cmd . "\n";
-
-print  . "\n";
 
 my @file_list;
 GetFilesInDirectory(\@file_list, $ENV{'PWD'}."/test/weekly");
@@ -59,6 +59,72 @@ sub GetFilesInDirectory
             {
                 $index++;
             }
+        }
+    }
+    return $success;
+}
+
+sub CreateArchive
+{
+    my ($archive_path, $root_dir, $sub_dir, $out_dir, $encryption) = @_;
+    my $success = 1;
+
+    my $tar_cmd;
+    my $tar_dest;
+
+    unless ( -e $out_dir )
+    {
+        print "ERROR: output directory \"$out_dir\" does not exist\n";
+        $success = 0;
+    }
+    unless ( -e $root_dir )
+    {
+        print "ERROR: source directory \"$root_dir\" does not exist\n";
+        $success = 0;
+    }
+    unless ( -e $root_dir )
+    {
+        print "ERROR: source directory \"$root_dir\" does not exist\n";
+        $success = 0;
+    }
+    if( $success == 1 )
+    {
+        my $source_file = "$root_dir/$sub_dir";
+        unless ( -e $source_file )
+        {
+            print "ERROR: source file \"$source_file\" does not exist\n";
+            $success = 0;
+        }
+    }
+
+    if($success == 1)
+    {
+        my $timestamp = strftime "%Y-%m-%d_%H-%M-GMT", gmtime time;
+
+        if($encryption == 1)
+        {
+            $tar_cmd = "$TAR $TAR_FLAGS_ENCRYPT";
+            $tar_dest = "$out_dir/$sub_dir-$timestamp.$TAR_EXT_ENCRYPT";
+        }
+        else
+        {
+            $tar_cmd = "$TAR $TAR_FLAGS";
+            $tar_dest = "$out_dir/$sub_dir-$timestamp.$TAR_EXT";
+        }
+
+        my $run_cd_cmd = "cd $root_dir";
+        my $run_tar_cmd = "$tar_cmd $tar_dest $sub_dir";
+        
+        my $run_cmd = "$run_cd_cmd && $run_tar_cmd";
+        print $run_cmd . "\n";
+        my $output = `$run_cmd`;
+
+        system($run_cmd);
+
+        unless ($? == 0)
+        {
+            $success = 0;
+            print "ERROR: tar command failed:\n$output\n";
         }
     }
     return $success;
